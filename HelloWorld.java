fce1b51d90150718fee523ebecb145718209bfd3
ginish314 <ginishkumardurg@gmail.com>
2025-07-16 10:46:44 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,23 +1,5 @@
-/* CallingMethodsInSameClass.java
- *
- * illustrates how to call static methods a class
- * from a method in the same class
- */
-
-public class CallingMethodsInSameClass
-{
-	public static void main(String[] args) {
-		printOne();
-		printOne();
-		printTwo();
-	}
-
-	public static void printOne() {
-		System.out.println("Hello World");
-	}
-
-	public static void printTwo() {
-		printOne();
-		printOne();
-	}
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
